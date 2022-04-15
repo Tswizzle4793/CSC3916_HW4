0f422885fbf5c3effe0e9a913beea7c13e530d54
tanner <tanner@wiechdoctor.net>
2022-04-15 13:49:38 -0600

    updates

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ router.get('/movies', function(req,res){
           if(err) {res.send(err);}
           else
             {
-                Movie.aggregate([{$lookup:{from: "reviews", localField: "title", foreignField: movie.title, as: "movie_reviews"}}]).then(values => res.json({msg:movie}));
+                Movie.aggregate([{$lookup:{from: "reviews", localField: "title", foreignField: movie.title, as: "movie_reviews"}}]).then(values => res.json({msg:movie + values}));
             }
             //res.json({msg:movie});
         })
