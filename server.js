cb01962a990e48d513efeb9a116d185549cba69d
tanner <tanner@wiechdoctor.net>
2022-04-15 10:12:02 -0600

    updates

--- server.js
+++ server.js
@@ -202,7 +202,16 @@ router.get('/movies', function(req,res){
     else if(req.query.title !== undefined && req.query.review !== undefined){
         Movie.aggregate([
             {
-                $match:{_id: req.query.title}},{$lookup: {from: "reviews", localField: "title", foreignField: "title", as: "movie_reviews"}
+                //$match:{_id: req.query.title}},{$lookup: {from: "reviews", localField: "title", foreignField: "title", as: "movie_reviews"}
+                $lookup:
+                    {
+                        from: "reviews",
+                        localField: "title",
+                        foreignField: "title",
+                        //pipeline: [{$group: {_id: "$title", avgRating: {$avg: "$rating"}}}],
+                        as: "movie_reviews",
+
+                    }
             }
 
         ]).then(values => res.json(values));
