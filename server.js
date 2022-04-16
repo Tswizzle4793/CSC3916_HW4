c360c205e99311763a7d9faef2877ae3ddb04dd6
tanner <tanner@wiechdoctor.net>
2022-04-16 13:39:15 -0600

    updates

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ router.get('/movies', function(req,res){
                     }
 
             },
-            {$unwind: "$movie_reviews"}, {$group:{_id:null, avgRating:{$avg: "$rating"}}}
+            {$unwind: "$movie_reviews"}, {$group:{_id:"$title", avgRating:{$avg: "$movie_reviews.rating"}}}
         ]).then(values => res.json(values));
     }
 
